685e01eb5d470975a2fc334240128186206e2d72
Prof Rafael <rafa.fernan10@gmail.com>
2020-08-03 16:40:33 -0300

    Corrigindo estrutura de arquivos

---
+++ AppCalc2/AppCalc/MainPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppCalc
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void zerar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maisMenos_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void porCento_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dividir_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num7_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num8_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num9_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void multiplicar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num4_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num5_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num6_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void subtrair_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
